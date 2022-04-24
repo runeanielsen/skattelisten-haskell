0d8f9d1ebc15d033077281d03457392f8c9b3621
Rune Nielsen <runenielsen@runbox.com>
2022-04-24 13:07:49 +0200

    now streams json to output

--- src/Main.hs
+++ src/Main.hs
@@ -1,39 +1,85 @@
 module Main where
 
-import Data.Aeson(encode, ToJSON(toJSON), object, (.=))
-import qualified Data.Text as T
-import Data.Char (toUpper)
+import qualified Basement.Types.CharUTF8 as B
 import Control.Monad (mzero)
-import System.IO (hGetLine, hIsEOF, hClose, utf8, hSetEncoding, openFile, IOMode(ReadMode), Handle)
-import Data.Text.Encoding (decodeUtf8)
+import Data.Aeson (ToJSON (toJSON), encode, object, (.=))
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Char (toUpper)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import RIO.ByteString.Lazy (unpack)
+import qualified RIO.ByteString.Lazy as B.CharUTF8
+import qualified RIO.ByteString.Lazy as Data.Aeson
+import System.IO
+  ( Handle,
+    IOMode (ReadMode, WriteMode),
+    hClose,
+    hGetLine,
+    hIsEOF,
+    hPrint,
+    hPutStrLn,
+    hSetEncoding,
+    openFile,
+    utf8,
+  )
 
-newtype Company = Company
-  { csv :: T.Text
-  } deriving Show
+data Company = Company
+  { csv :: T.Text,
+    name :: T.Text,
+    se :: T.Text,
+    income_year :: T.Text,
+    company_type :: T.Text,
+    taxable_income :: T.Text,
+    deficit :: T.Text,
+    corporate_tax :: T.Text
+  }
+  deriving (Show)
 
 instance ToJSON Company where
-    toJSON (Company csv) =
-      object [ T.pack "csv" .= csv ]
+  toJSON (Company csv name se income_year company_type taxable_income deficit corporate_tax) =
+    object
+      [ T.pack "csv" .= csv,
+        T.pack "name" .= name,
+        T.pack "se" .= se,
+        T.pack "income_year" .= income_year,
+        T.pack "company_type" .= company_type,
+        T.pack "taxable_income" .= taxable_income,
+        T.pack "deficit" .= deficit,
+        T.pack "corporate_tax" .= corporate_tax
+      ]
 
 main :: IO ()
 main = do
-  inh <- openFile "/home/notation/Downloads/skatteliste-2019.csv" ReadMode
+  inh <- openFile "" ReadMode
+  ouh <- openFile "" WriteMode
   hSetEncoding inh utf8
-  mainLoop inh
-  putStrLn "Finished"
+  _ <- hGetLine inh -- Skip first line
+  mainLoop inh ouh
   hClose inh
+
+createCompany :: [T.Text] -> Company
+createCompany line =
+  Company
+    (head line)
+    (line !! 1)
+    (line !! 2)
+    (line !! 3)
+    (line !! 5)
+    (line !! 8)
+    (line !! 9)
+    (line !! 10)
 
 splitCSV :: T.Text -> [T.Text]
 splitCSV =
   T.splitOn $ T.pack ","
 
-mainLoop :: Handle -> IO ()
-mainLoop inh = do
+mainLoop :: Handle -> Handle -> IO ()
+mainLoop inh ouh = do
   ineof <- hIsEOF inh
   if ineof
     then return ()
     else do
-    inpStr <- hGetLine inh
-    let company_json = encode . Company . head . splitCSV . T.pack $ inpStr
-    print $ show company_json
-    mainLoop inh
+      inpStr <- hGetLine inh
+      let company_json = encode . createCompany . splitCSV . T.pack $ inpStr
+      hPutStrLn ouh $ B.unpack company_json
+      mainLoop inh ouh
